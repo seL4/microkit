6c16e4bb7ec66a52afe1360e62b1c95b7a08e154
Zoltan A. Kocsis <11808286+zaklogician@users.noreply.github.com>
2022-03-25 13:49:32 +1100

    Integrate extra requirements to install_capdl.sh
    Signed-off-by: Zoltan A. Kocsis <11808286+zaklogician@users.noreply.github.com>

--- install_capdl.sh
+++ install_capdl.sh
@@ -5,3 +5,4 @@ git clone https://github.com/seL4/capdl.git
 cd -
 cp capdl_pip.txt ../capdl/python-capdl-tool/setup.py
 ./pyenv/bin/python3.9 -m pip install ../capdl/python-capdl-tool/
+./pyenv/bin/python3.9 -m pip install -r capdl_requirements.txt
