fe75883e9773202bb68b62fd2ffb976971a8b91c
Lucy <lucyparker544@gmail.com>
2022-08-15 13:53:21 +1000

    Basic example with a passive server
    Signed-off-by: Lucy <lucyparker544@gmail.com>

---
+++ example/imx8mm/passive_server/client.c
@@ -0,0 +1,20 @@
+
+#include <sel4cp.h>
+
+#define SERVER_CH 0 
+
+void
+init(void)
+{
+    sel4cp_dbg_puts("client: client protection domain init function running\n");
+
+    /* message the server */
+    sel4cp_mr_set(0, 0);
+    (void) sel4cp_ppcall(SERVER_CH, sel4cp_msginfo_new(1, 1));
+}
+
+void
+notified(sel4cp_channel ch)
+{
+    sel4cp_dbg_puts("Client recieved a notification on an unexpected channel\n");
+}
