591d3d1312e823ecbfe60a837b4a3046ddef5076
Ivan Velickovic <i.velickovic@unsw.edu.au>
2022-11-29 15:24:07 +1100

    Temporary hack to prevent the bootloader from corrupting data

--- tool/sel4coreplat/util.py
+++ tool/sel4coreplat/util.py
@@ -163,9 +163,8 @@ class DisjointMemoryRegion:
         # 'best' may be something that best matches a power-of-two
         # allocation
         for region in self._regions:
-             # @ivanv: HACK, I think the loader when builidng the imx8mm vmm system will overwrite itself so we need this.
-             # if size <= region.size and region.base >= 0x60000000:
-             if size <= region.size: 
+            # @ivanv: HACK, I think the loader when builidng the imx8mm vmm system will overwrite itself so we need this.
+            if size <= region.size and region.base >= 0x60000000:
                 break
         else:
             raise ValueError(f"Unable to allocate 0x{size:x} bytes.")
