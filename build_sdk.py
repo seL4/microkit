67f451652226416ed3af98f00c0532d8656f7667
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2023-06-14 22:41:08 +1000

    Re-enable building of manual, try get macOS CI to build manual as well

--- build_sdk.py
+++ build_sdk.py
@@ -835,7 +835,7 @@ def main() -> None:
         test_tool()
         build_tool(tool_target, args.tool_target_triple)
 
-    # build_doc(root_dir)
+    build_doc(root_dir)
 
     build_dir = Path("build")
     for board in selected_boards:
