337a5bb18583bc46162ebc2959b695e903f4d3da
Ivan Velickovic <i.velickovic@unsw.edu.au>
2023-09-13 19:53:10 +1000

    Adhere to style checks in passive server example
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- example/imx8mm/passive_server/server.c
+++ example/imx8mm/passive_server/server.c
@@ -6,29 +6,26 @@
 
 #include <microkit.h>
 
-microkit_msginfo
-protected(microkit_channel ch, microkit_msginfo msginfo)
+microkit_msginfo protected(microkit_channel ch, microkit_msginfo msginfo)
 {
     switch (microkit_msginfo_get_label(msginfo)) {
-        case 0:
-            microkit_dbg_puts("server: is running on clients scheduling context\n");
-            break;
-        default:
-            microkit_dbg_puts("server: received an unexpected message\n");
+    case 0:
+        microkit_dbg_puts("server: is running on clients scheduling context\n");
+        break;
+    default:
+        microkit_dbg_puts("server: received an unexpected message\n");
     }
 
     return seL4_MessageInfo_new(0, 0, 0, 0);
 }
 
-void
-init(void)
+void init(void)
 {
     microkit_dbg_puts("server: server protection domain init function running\n");
     /* Nothing to initialise */
 }
 
-void
-notified(microkit_channel ch)
+void notified(microkit_channel ch)
 {
     microkit_dbg_puts("server: recieved a notification on an unexpected channel\n");
 }
