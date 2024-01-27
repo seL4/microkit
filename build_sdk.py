96f30124a942bd17a39751dd6aebdbee8835e143
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-01-27 18:08:21 +1100

    Re-enable support for i.MX8MM-EVK
    The version of seL4 that Microkit used previously failed to build
    for the i.MX8MM-EVK which is why this was disabled.
    
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- build_sdk.py
+++ build_sdk.py
@@ -94,19 +94,19 @@ SUPPORTED_BOARDS = (
             "hello": Path("example/maaxboard/hello")
         }
     ),
-    # BoardInfo(
-    #     name="imx8mm",
-    #     gcc_cpu="cortex-a53",
-    #     loader_link_address=0x41000000,
-    #     kernel_options={
-    #         "KernelPlatform": "imx8mm-evk",
-    #         "KernelIsMCS": True,
-    #         "KernelArmExportPCNTUser": True,
-    #     },
-    #     examples={
-    #         "passive_server": Path("example/imx8mm/passive_server")
-    #     }
-    # )
+    BoardInfo(
+        name="imx8mm",
+        gcc_cpu="cortex-a53",
+        loader_link_address=0x41000000,
+        kernel_options={
+            "KernelPlatform": "imx8mm-evk",
+            "KernelIsMCS": True,
+            "KernelArmExportPCNTUser": True,
+        },
+        examples={
+            "passive_server": Path("example/imx8mm/passive_server")
+        }
+    )
 )
 
 SUPPORTED_CONFIGS = (
