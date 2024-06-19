697f63769fa8b87f119c351184e91f47dc838005
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-06-19 12:44:13 +1000

    Add hierarchy example to show off PD hierarchy
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

---
+++ example/qemu_virt_aarch64/hierachy/crasher.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2021, Breakaway Consulting Pty. Ltd.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+#include <stdint.h>
+#include <microkit.h>
+
+void init(void)
+{
+    int *x = 0;
+    microkit_dbg_puts("crasher, starting\n");
+    /* Crash! */
+    *x = 1;
+}
+
+void notified(microkit_channel ch)
+{
+}
