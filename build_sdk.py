df062433f017193b6fa86955ee9357df3ea792f3
Mathieu Mirmont <mat@neutrality.ch>
2024-10-24 15:04:34 +0200

    Add the ODROID H2+ x86 board
    Signed-off-by: Mathieu Mirmont <mat@neutrality.ch>

--- build_sdk.py
+++ build_sdk.py
@@ -307,6 +307,24 @@ SUPPORTED_BOARDS = (
             "hello": Path("example/x86_64_nehalem/hello")
         }
     ),
+    BoardInfo(
+        name="x86_64_goldmont_plus",
+        arch=KernelArch.X86_64,
+        gcc_cpu=None,
+        gcc_march="goldmont-plus",
+        loader_link_address=0x10000000, # 256MB
+        kernel_options = {
+            "KernelIsMCS": True,
+            "KernelPlatform": "pc99",
+            "KernelSel4Arch": "x86_64",
+            "KernelVTX": True,
+            "KernelX86MicroArch": "goldmont-plus",
+            "KernelSupportPCID": False,
+        },
+        examples = {
+            "hello": Path("example/x86_64_goldmont_plus/hello")
+        }
+    ),
 )
 
 SUPPORTED_CONFIGS = (
