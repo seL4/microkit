3482977ee0d087bc2e6511390acacff159b53ca0
Hesham Almatary <heshamalmatary@capabilitieslimited.co.uk>
2025-06-05 18:52:39 +0000

    dev_build.py: Support building with LLVM
    Add a new --llvm flag to enable building with LLVM toolchain.
    
    Also fix a trivial error adding single quotes within a string.
    
    Signed-off-by: Hesham Almatary <heshamalmatary@capabilitieslimited.co.uk>

--- dev_build.py
+++ dev_build.py
@@ -64,6 +64,11 @@ def main():
         default="debug",
         help="Config option to be passed to the tool"
     )
+    parser.add_argument(
+        "--llvm",
+        action="store_true",
+        help="Build with LLVM/Clang toolchain"
+    )
     args = parser.parse_args()
 
     # TODO: Support choosing a release by specifying on command line
@@ -76,7 +81,7 @@ def main():
     if not BUILD_DIR.exists():
         BUILD_DIR.mkdir()
 
-    tool_rebuild = f"cd {CWD / "tool/microkit"} && cargo build --release"
+    tool_rebuild = f"cd {CWD / 'tool/microkit'} && cargo build --release"
     r = system(tool_rebuild)
     assert r == 0
 
@@ -86,6 +91,7 @@ def main():
     make_env["MICROKIT_CONFIG"] = args.config
     make_env["MICROKIT_SDK"] = str(release)
     make_env["MICROKIT_TOOL"] = (CWD / "tool/microkit/target/release/microkit").absolute()
+    make_env["LLVM"] = str(args.llvm)
 
     # Choose the makefile based on the `--example-from-sdk` command line flag
     makefile_directory = (
