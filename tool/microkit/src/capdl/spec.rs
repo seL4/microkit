90aac53e73fc87f0cba4a968e8205a70a8688371
Jakub Duchniewicz <j.duchniewicz@unsw.edu.au>
2026-04-10 16:11:01 +1000

    Add support for FPU flag for PDs
    Signed-off-by: Jakub Duchniewicz <j.duchniewicz@unsw.edu.au>

--- tool/microkit/src/capdl/spec.rs
+++ tool/microkit/src/capdl/spec.rs
@@ -85,6 +85,7 @@ pub fn capdl_obj_human_name(obj: &Object<FrameFill>, sel4_config: &Config) -> &'
         Object::ArmSmc => "ARM SMC",
         Object::Untyped(_) => "Untyped",
         Object::Irq(_) => "IRQ",
+        Object::DomainSet => "DomainSet",
     }
 }
 
