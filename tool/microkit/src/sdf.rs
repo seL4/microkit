b823343df80f1181b50d15f7660f36ab495bdb81
Callum <c.berry@student.unsw.edu.au>
2026-07-06 21:21:41 +1000

    IOMMU: Minor modification to Map trait
    Minor adjustments to the prior sdf work to do with how permissions are
    handled.  Previously I implemented read and write functions as part of
    the SysIOMapPerms enum, however the old SysMapPerms did not do this.
    Given these types are meant to exhibit similar behaviour extracting this
    common logic into the Map trait provides a streamlined interface to
    check if a permission is set.
    
    Signed-off-by: Callum <c.berry@student.unsw.edu.au>

--- tool/microkit/src/sdf.rs
+++ tool/microkit/src/sdf.rs
@@ -306,14 +306,6 @@ impl SysIOMapPerms {
             (false, false) => Err(()),
         }
     }
-
-    pub fn read(self) -> bool {
-        matches!(self, SysIOMapPerms::Read | SysIOMapPerms::ReadWrite)
-    }
-
-    pub fn write(self) -> bool {
-        matches!(self, SysIOMapPerms::Write | SysIOMapPerms::ReadWrite)
-    }
 }
 
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -327,13 +319,17 @@ pub struct SysIOMap {
     pub text_pos: Option<roxmltree::TextPos>,
 }
 
-trait Map {
+pub trait Map {
     fn mr_name(&self) -> &str;
     fn addr(&self) -> u64;
     fn text_pos(&self) -> Option<roxmltree::TextPos>;
     fn element(&self) -> &'static str;
     fn addr_name(&self) -> &'static str;
     fn range_name(&self) -> &'static str;
+    fn read(&self) -> bool;
+    fn write(&self) -> bool;
+    fn execute(&self) -> bool;
+    fn cached(&self) -> bool;
 }
 
 impl Map for SysMap {
@@ -359,6 +355,22 @@ impl Map for SysMap {
 
     fn range_name(&self) -> &'static str {
         "virtual address range"
+    }
+
+    fn read(&self) -> bool {
+        self.perms & SysMapPerms::Read as u8 != 0
+    }
+
+    fn write(&self) -> bool {
+        self.perms & SysMapPerms::Write as u8 != 0
+    }
+
+    fn execute(&self) -> bool {
+        self.perms & SysMapPerms::Execute as u8 != 0
+    }
+
+    fn cached(&self) -> bool {
+        self.cached
     }
 }
 
@@ -386,7 +398,24 @@ impl Map for SysIOMap {
     fn range_name(&self) -> &'static str {
         "io address range"
     }
+
+    fn read(&self) -> bool {
+        matches!(self.perms, SysIOMapPerms::Read | SysIOMapPerms::ReadWrite)
+    }
+
+    fn write(&self) -> bool {
+        matches!(self.perms, SysIOMapPerms::Write | SysIOMapPerms::ReadWrite)
+    }
+
+    fn execute(&self) -> bool {
+        false
+    }
+
+    fn cached(&self) -> bool {
+        false
+    }
 }
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum SysMemoryRegionKind {
     User,
